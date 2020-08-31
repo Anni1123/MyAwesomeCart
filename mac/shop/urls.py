6c82b9ab8e8162939b3c2845f548070dc06c7f43
Anni1123 <kumarianni1213@gmail.com>
2020-08-31 23:06:53 +0530

    Cart&Contact PageAdded

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.index,name="index"),
-    path('about/', views.about, name="AboutUs"),
-    path('contact/', views.contact, name="ContactUs"),
-    path('tracker/', views.tracker, name="Tracking Status"),
-    path('search/', views.search, name="Search"),
-    path('productView/', views.productView, name="Product View"),
-    path('checkout/', views.checkout, name="CheckOut"),
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("search/", views.search, name="Search"),
+    path("products/<int:myid>", views.productView, name="ProductView"),
+    path("checkout/", views.checkout, name="Checkout"),
 ]
